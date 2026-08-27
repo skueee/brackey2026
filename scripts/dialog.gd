ce92f47078967261c7484e6bf58350af98e4499e
skueee <skueee@outlook.com>
2026-08-27 19:03:27 +0200

    Added characters support in game

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -1,16 +1,29 @@
 extends Control
 
-var active : bool = false
+signal dialog_finished
+signal question_finished(value: bool)
+
+var dialog_active : bool = false
 var current_text : Array
 var next_text_index : int
+
+var question_active : bool = false
+var current_choice : bool = true
+var question_active_color : Color = Color("000000")
+var question_inactive_color : Color = Color("4f4f4f")
 
 @onready var _speaker : Label = $Dialog/VBoxContainer/SpeakerBox/Label
 @onready var _dialogue : RichTextLabel = $Dialog/VBoxContainer/TextBox/Text
 @onready var _continue : Control = $Dialog/VBoxContainer/TextBox/Continue
 
+@onready var _question : Label = $Choice/VBoxContainer/Question/QuestionLabel
+@onready var _first_choice : Label = $Choice/VBoxContainer/Choice/HBoxContainer/FirstChoiceLabel
+@onready var _second_choice : Label = $Choice/VBoxContainer/Choice/HBoxContainer/SecondChoiceLabel
+
 func _ready() -> void:
 	$HideScreen.hide()
 	$Dialog.hide()
+	$Choice.hide()
 
 func displayLine(line : String):
 	_dialogue.text = line
@@ -22,16 +35,17 @@ func displayText(lines : Array, speaker : String = "", hide_bg: bool = false):
 	next_text_index = 1
 	
 func showTextBox(hide_bg : bool, speaker : String = ""):
-	active = true
+	dialog_active = true
 	$Dialog.visible = true
 	$HideScreen.visible = hide_bg
 	_speaker.visible = (speaker != "")
 	_speaker.text = speaker
 	
 func hideTextBox():
-	active = false
+	dialog_active = false
 	$Dialog.visible = false
 	$HideScreen.visible = false
+	dialog_finished.emit()
 
 func goToNextText():
 	if next_text_index < current_text.size():
@@ -40,6 +54,36 @@ func goToNextText():
 	else:
 		hideTextBox()
 
+func displayChoice(question: String, choicea: String, choiceb: String, hide_bg: bool = false):
+	question_active = true
+	current_choice = true
+	$Choice.visible = true
+	$HideScreen.visible = hide_bg
+	_question.text = question
+	_first_choice.text = choicea
+	_first_choice.add_theme_color_override("font_color", question_active_color)
+	_second_choice.text = choiceb
+	_second_choice.add_theme_color_override("font_color", question_inactive_color)
+	
+func nextChoice():
+	current_choice = !current_choice
+	if current_choice == true:
+		_first_choice.add_theme_color_override("font_color", question_active_color)
+		_second_choice.add_theme_color_override("font_color", question_inactive_color)
+	else:
+		_second_choice.add_theme_color_override("font_color", question_active_color)
+		_first_choice.add_theme_color_override("font_color", question_inactive_color)
+		
+func choiceInteract():
+	question_active = false
+	$Choice.visible = false
+	$HideScreen.visible = false
+	question_finished.emit(current_choice)
+
 func _process(_delta: float) -> void:
-	if active and Input.is_action_just_pressed("text_continue"):
+	if dialog_active and Input.is_action_just_pressed("text_continue"):
 		goToNextText()
+	if question_active and Input.is_action_just_pressed("question_choose"):
+		nextChoice()
+	if question_active and Input.is_action_just_pressed("question_enter"):
+		choiceInteract()
--- scripts/maindoor.gd
+++ scripts/maindoor.gd
@@ -4,11 +4,76 @@ var someone_is_there : bool = false
 var current_person_id : int
 
 # Group 2 as way fewer chances to get selected
-var group1 = []
-var group2 = []
+var group1 : Array = [1]
+var group2 : Array
+
+func selectRandomPerson():
+	var to_random : Array
+	to_random.append_array(group1)
+	to_random.append_array(group1)
+	to_random.append_array(group1)
+	to_random.append_array(group2)
+	return to_random.pick_random()
+	
+func enablePerson():
+	current_person_id = selectRandomPerson()
+	someone_is_there = true
+	if group1.has(current_person_id):
+		group1.erase(current_person_id)
+		group2.append(current_person_id)
+		
+func disablePerson():
+	current_person_id = 0
+	someone_is_there = false
 
 func interact():
 	if someone_is_there:
-		pass
+		executeCharacter(current_person_id)
 	else:
 		get_node("/root/Game/Player/HUD").DisplayText("No one is there", 1, 0.8)
+
+func executeCharacter(id):
+	var Character = load("res://scripts/characters/" + str(id) + ".gd")
+	var character = Character.new()
+	var dialog = get_node("/root/Game/Dialog")
+	
+	dialog.displayText(character.dialogues.slice(0, (character.dialogues.size() - 2)), character.character_name, true)
+	await dialog.dialog_finished
+	dialog.displayChoice("Accept ?", character.dialogues[-2], character.dialogues[-1], true)
+	var choice: bool = await dialog.question_finished
+	if choice == true:
+		executeActions(character.next_action[0], character)
+	else:
+		executeActions(character.next_action[1], character)
+	
+func executeActions(actions: Array, character):
+	var dialog = get_node("/root/Game/Dialog")
+	var player = get_node("/root/Game/Player")
+	
+	for i in actions:
+		if i[0] == "dialog":
+			dialog.displayText(i[1], character.character_name, true)
+			await dialog.dialog_finished
+		elif i[0] == "function":
+			Callable.create(character, i[1]).call
+		elif i[0] == "give_object":
+			pass
+		elif i[0] == "change_health":
+			if i[1] > 0:
+				player.healDamage(i[1])
+			else:
+				player.dealDamage(i[1])
+		elif i[0] == "change_hungry":
+			if i[1] > 0:
+				player.healHungry(i[1])
+			else:
+				player.dealHungry(i[1])
+		elif i[0] == "change_drink":
+			if i[1] > 0:
+				player.healDrink(i[1])
+			else:
+				player.dealDrink(i[1])
+	
+func _ready() -> void:
+	enablePerson()
+	
